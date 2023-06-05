d6fd4bd9701b62336614f7901fe1326fec971beb
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-06-05 13:05:01 -0400

    add files and ride class

---
+++ lib/ride.rb
@@ -0,0 +1,23 @@
+class Ride
+    attr_reader :name, :distance, :terrain
+  
+    def initialize(ride_specs)
+      @name = ride_specs[:name]
+      @distance = ride_specs[:distance]
+      @loop = ride_specs[:loop]
+      @terrain = ride_specs[:terrain]
+    end
+  
+    def loop?
+      @loop
+    end
+  
+    def total_distance
+      if loop?
+        @distance 
+      else 
+        @distance * 2
+      end
+  end
+  end
+  
